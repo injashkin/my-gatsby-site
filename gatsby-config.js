b5a685d9c2149dcd16ae86ace382880ac3a015c9
injashkin <injashkin@gmail.com>
2022-11-01 14:18:48 +0300

    Install and configure plugins for images

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `My Gatsby Site`,
+    description: 'Example project for the Gatsby Head API',
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
-}
+  plugins: ['gatsby-plugin-image', 'gatsby-plugin-sharp'],
+};
