7a470cfb70a1af5a7cdd2a22654b2efa60154b21
injashkin <injashkin@gmail.com>
2022-11-02 17:52:48 +0300

    Update blog.js
    На странице Blog отображается список статей в обратном
    хронологическом порядке. Для каждой статьи выводится заголовок,
    дата и её содержимое.

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,20 +6,28 @@ import Seo from '../components/seo';
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Post">
-      <ul>
-        {data.allFile.nodes.map((node) => (
-          <li key={node.name}>{node.name}</li>
-        ))}
-      </ul>
+      {data.allMdx.nodes.map((node) => (
+        <article key={node.id}>
+          {node.name}
+          <h2>{node.frontmatter.title}</h2>
+          <p>Posted: {node.frontmatter.date}</p>
+          <p>{node.excerpt}</p>
+        </article>
+      ))}
     </Layout>
   );
 };
 
 export const query = graphql`
   query {
-    allFile {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
       nodes {
-        name
+        frontmatter {
+          date(formatString: "D-MMMM-YYYY")
+          title
+        }
+        id
+        excerpt
       }
     }
   }
